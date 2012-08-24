c6ae381a0e9e55448d9858830b565404e6c2ede1
zhangyang09 <zhangyang09@snda.com>
2012-08-24 11:49:36 +0800

    Add port_forward/datapipe to support log feature

---
+++ port_forward/datapipe/client.py
@@ -0,0 +1,13 @@
+import socket
+def client(string):
+    HOST, PORT = '127.0.0.1', 11111
+    # SOCK_STREAM == a TCP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #sock.setblocking(0)  # optional non-blocking
+    sock.connect((HOST, PORT))
+    sock.send(string)
+    reply = sock.recv(16384)  # limit reply to 16K
+    sock.close()
+    return reply
+
+assert client('2+2') == '4'
